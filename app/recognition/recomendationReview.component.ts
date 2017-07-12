b6b48c0acbc17f57d783549ffda8ff8555d8be59
Lokesh Nagpal <lokeshnagpal86@gmail.com>
2017-07-12 16:39:13 +0530

    update files

--- app/recognition/recomendationReview.component.ts
+++ app/recognition/recomendationReview.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
-
+import { Router } from "@angular/router";
+ 
 @Component({
    selector:'revRecom',
    templateUrl:'./app/recognition/recomedationReview.html'
 })
 
-export class ReviewRecomendation{}
+export class ReviewRecomendation{
+    constructor(private router:Router){}
+    update(){
+      this.router.navigate(['/postrecomendation']);
+    }
+}
